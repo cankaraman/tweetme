7f8f692a97b5951e8838430e60955a8929d9bd1d
can karaman <ycankaraman@gmail.com>
2019-09-21 18:26:56 +0300

    added date display to tweets

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 from tweets.models import Tweet
 from accounts.api.serializers import UserDisplaySerializer
+from django.utils.timesince import timesince
+
 
 class TweetModelSerializer(serializers.ModelSerializer):
-    user = UserDisplaySerializer()
+    user = UserDisplaySerializer(read_only=True)
+    date_display = serializers.SerializerMethodField()
+    timesince = serializers.SerializerMethodField()
+
     class Meta:
         model = Tweet
         fields = [
             "user",
-            "content"
+            "content",
+            "timestamp",
+            "date_display",
+            "timesince",
+
         ]
+
+    def get_date_display(self, obj):
+        return obj.timestamp.strftime("%d %b %Y, %I:%m %p")
+
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp) + " ago"
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -2,13 +2,22 @@ from rest_framework import generics
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 from django.db.models import Q
+from rest_framework import permissions
 
 
-class TweetListAPIview(generics.ListAPIView):
+class TweetCreateAPIView(generics.CreateAPIView):
+    serializer_class = TweetModelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
     def get_queryset(self, *args, **kwargs):
-        qs = Tweet.objects.all()
+        qs = Tweet.objects.all().order_by("-timestamp")
         query = self.request.GET.get("q", None)
         if query is not None:
             qs = qs.filter(
