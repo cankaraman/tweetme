b74fc3dc515dc41d3468bfdc8545680aecbb29d3
can karaman <ycankaraman@gmail.com>
2020-02-19 15:04:03 +0300

    retweet via ajax and add js username parsing

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url
-from .views import TweetListAPIView, TweetCreateAPIView
+from .views import TweetListAPIView, TweetCreateAPIView, RetweetAPIView
 # from django.views.generic.base import RedirectView
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'),
     url(r'^create$', TweetCreateAPIView.as_view(), name='create'),
-    # url(r'^$',RedirectView.as_view(url="/")),
-    # url(r'^search/$', TweetListView.as_view(), name='list'),
-    # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
-    # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
-    # url(r'^(?P<pk>\d+)/delete', TweetDeleteView.as_view(), name='delete'),
+    url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),  # tweet/api/id/retweet
 ]
